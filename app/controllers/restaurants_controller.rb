aa0be778e907cb9b389f868f4d7f6804ad8ce945
jbudrow <jbudrowd3@yahoo.com>
2016-06-24 15:39:19 -0400

    set user permissions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,7 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :check_user, except: [:index, :show]
 
   # GET /restaurants
   # GET /restaurants.json
@@ -78,4 +80,9 @@ class RestaurantsController < ApplicationController
     def restaurant_params
       params.require(:restaurant).permit(:name, :address, :phone, :website, :image)
     end
+
+    def check_user
+      redirect_to root_url, alert: "Sorry, only admins can do that!"
+    end
+  end
 end
