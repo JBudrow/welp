1b4ffd2b74f08126acb02b7a966d634029e0e03e
jbudrow <jbudrowd3@yahoo.com>
2016-06-24 11:47:16 -0400

    link reviews to restaurants, belongsto-hasmany relationship

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
-
-  # GET /reviews
-  # GET /reviews.json
-  def index
-    @reviews = Review.all
-  end
-
-  # GET /reviews/1
-  # GET /reviews/1.json
-  def show
-  end
+  before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :set_restaurant
 
   # GET /reviews/new
   def new
@@ -26,11 +16,12 @@ class ReviewsController < ApplicationController
   # POST /reviews.json
   def create
     @review = Review.new(review_params)
-    @review.user_id = current_user.id 
+    @review.user_id = current_user.id
+    @review.restaurant_id = @restaurant_id
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
